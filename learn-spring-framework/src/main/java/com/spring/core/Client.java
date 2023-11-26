d558a253f4ec8a298b2c0ddf72c1944b065a7d3c
Thilanka Weerasekara <thilankapathum@gmail.com>
2023-11-26 20:14:26 +0530

    completed how spring-ioc-container works

--- learn-spring-framework/src/main/java/com/spring/core/Client.java
+++ learn-spring-framework/src/main/java/com/spring/core/Client.java
@@ -1,17 +1,37 @@
 package com.spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Client {
     public static void main(String[] args) {
 
         // Creating a Car Object using interface type, Vehicle
-        Vehicle vehicle = new Car();
-//        vehicle = new Cycle();
+        // Vehicle vehicle = new Car();
 
-//        Vehicle vehicle = new Bike();  //Can substitute Car Object into a Bike Object like this
+        // vehicle = new Cycle();
+        // Vehicle vehicle = new Bike();  //Can substitute Car Object into a Bike Object like this
 
         // New traveller Object is created using the newly created Car Object (via Vehicle instance).
         // Traveler's constructor requires a Vehicle Object as an argument.
-        Traveler traveler = new Traveler(vehicle);
-        traveler.startJourney();
+        // Traveler traveler = new Traveler(vehicle);
+        // traveler.startJourney();
+
+        // Creating Spring IOC container.
+        // Spring IOC container will read AppConfig.class for required dependencies/configurations
+        // Based on configuration it'll create and manage Spring beans
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Retrieve Spring beans from Spring IOC container
+        Car car = applicationContext.getBean(Car.class);
+        car.move();
+
+        Bike bike = applicationContext.getBean(Bike.class);
+        bike.move();
+
+        Traveler traveler1 = applicationContext.getBean(Traveler.class);
+        traveler1.startJourney();
+
+
     }
 }
