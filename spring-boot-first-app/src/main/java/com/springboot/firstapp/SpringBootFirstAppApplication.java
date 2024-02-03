f078ea6ddb7aea17194e32c91b0efe9b70d5d01d
Thilanka Weerasekara <thilankapathum@gmail.com>
2024-02-04 00:40:41 +0530

    Started REST API - Hello World

--- spring-boot-first-app/src/main/java/com/springboot/firstapp/SpringBootFirstAppApplication.java
+++ spring-boot-first-app/src/main/java/com/springboot/firstapp/SpringBootFirstAppApplication.java
@@ -1,5 +1,6 @@
 package com.springboot.firstapp;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootFirstAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootFirstAppApplication.class, args);
 	}
 
