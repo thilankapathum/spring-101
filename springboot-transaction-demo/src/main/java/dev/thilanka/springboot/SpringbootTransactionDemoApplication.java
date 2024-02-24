c415c1372995b96cbc2d68fc84630df3021e2e8c
Thilanka Weerasekara <thilankapathum@gmail.com>
2024-02-24 16:52:38 +0530

    Spring Boot Transaction Demo Start

---
+++ springboot-transaction-demo/src/main/java/dev/thilanka/springboot/SpringbootTransactionDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.thilanka.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTransactionDemoApplication.class, args);
+	}
+
+}
