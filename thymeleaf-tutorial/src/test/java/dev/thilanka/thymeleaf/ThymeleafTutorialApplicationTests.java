39540c69d735c5bf1c9671f4e274e5d5554264e5
Thilanka Weerasekara <thilankapathum@gmail.com>
2024-03-09 13:08:16 +0530

    Thymeleaf Example start

---
+++ thymeleaf-tutorial/src/test/java/dev/thilanka/thymeleaf/ThymeleafTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thilanka.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
