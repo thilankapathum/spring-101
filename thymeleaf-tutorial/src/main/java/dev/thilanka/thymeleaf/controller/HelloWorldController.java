9f672ec0970e6ca874adf9df2dc4116f327d5e61
Thilanka Weerasekara <thilankapathum@gmail.com>
2024-03-10 15:24:46 +0530

    Modified Dockerfile and docker-compose.yml

--- thymeleaf-tutorial/src/main/java/dev/thilanka/thymeleaf/controller/HelloWorldController.java
+++ thymeleaf-tutorial/src/main/java/dev/thilanka/thymeleaf/controller/HelloWorldController.java
@@ -1,2 +1,19 @@
-package dev.thilanka.thymeleaf.controller;public class HelloWorldController {
+package dev.thilanka.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    //-- Handler method to handle helloworld request
+    //-- http://localhost:3306/hello-world
+    @GetMapping("hello-world")
+    public String helloWorld(Model model){
+        model.addAttribute("message","HelloWorld!");
+        return "hello-world";
+    }
+
+
 }
